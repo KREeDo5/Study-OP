2bff73007593c5417cc49794b9140c7471cb8a05
Dmitry <mitya.lyashko.02@mail.ru>
2022-10-02 11:47:21 +0300

    Update code page IP.pas

--- LW_5/IndependentPractice/IP.pas
+++ LW_5/IndependentPractice/IP.pas
@@ -12,18 +12,18 @@ BEGIN
       IF Ch1 <= Ch3
       THEN
         WRITELN(Ch1, Ch3, Ch2)
-      ELSE{Ch3 < Ch1, Ch3 < Ch2, Ch1 <= Ch2}
+      ELSE {Ch3 < Ch1, Ch3 < Ch2, Ch1 <= Ch2}
         WRITELN(Ch3, Ch1, Ch2)
-    ELSE{Ch1 <= Ch2, Ch2 <= Ch3}
+    ELSE {Ch1 <= Ch2 <= Ch3}
       WRITELN(Ch1, Ch2, Ch3)     
-  ELSE{Ch2 < Ch1}
+  ELSE {Ch2 < Ch1}
     IF Ch3 < Ch1
     THEN 
       IF Ch2 <= Ch3
       THEN
         WRITELN(Ch2, Ch3, Ch1)
-      ELSE{Ch3 < Ch2, Ch2 < Ch1}
+      ELSE {Ch3 < Ch2 < Ch1}
         WRITELN(Ch3, Ch2, Ch1)
-    ELSE{Ch2 < Ch1, Ch1 <= Ch3}
+    ELSE {Ch2 < Ch1 <= Ch3}
       WRITELN(Ch2, Ch1, Ch3)
 END.
