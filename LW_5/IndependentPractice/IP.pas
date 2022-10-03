118a28d79da783fe4eed6b0f7c365d6fb82bd490
Dmitry <mitya.lyashko.02@mail.ru>
2022-10-03 21:39:07 +0300

    Update code page IP.pas; Added 51.pas

--- LW_5/IndependentPractice/IP.pas
+++ LW_5/IndependentPractice/IP.pas
@@ -2,9 +2,9 @@ PROGRAM Pyziryok(INPUT, OUTPUT);
 VAR
   Ch1, Ch2, Ch3: CHAR;
 BEGIN
-  READ(Ch1);
-  READ(Ch2);
-  READ(Ch3);
+  READ(Ch1, Ch2, Ch3);
+  WRITELN('Input data: ', Ch1, Ch2, Ch3);
+  WRITE('Sorted data by bubble sorting method : ');
   IF Ch1 <= Ch2
   THEN
     IF Ch3 < Ch2
@@ -15,10 +15,10 @@ BEGIN
       ELSE {Ch3 < Ch1, Ch3 < Ch2, Ch1 <= Ch2}
         WRITELN(Ch3, Ch1, Ch2)
     ELSE {Ch1 <= Ch2 <= Ch3}
-      WRITELN(Ch1, Ch2, Ch3)     
+      WRITELN(Ch1, Ch2, Ch3)
   ELSE {Ch2 < Ch1}
     IF Ch3 < Ch1
-    THEN 
+    THEN
       IF Ch2 <= Ch3
       THEN
         WRITELN(Ch2, Ch3, Ch1)
